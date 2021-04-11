0017bcbed0d9e1116d7fbce62e2bef0411c0fb4a
Michał Mroczkowski <mchlmroczkowski@gmail.com>
2021-04-11 22:12:09 +0200

    Deleting else

--- funkcja palin.py
+++ funkcja palin.py
@@ -1,7 +1,6 @@
 def palindromy(word):
- txt = word[::-1]
- if word == txt:
-   return True
- else:
-   return False
-print(palindromy('warka'))
+    txt = word[::-1]
+    if word == txt:
+        return True
+    return False
+print(palindromy('zakaz'))
